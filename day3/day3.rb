3c8251f754dd8e15279652e523f6760375362b82
David Webster <david.webster@postmates.com>
2018-12-04 15:37:45 -0800

    Complete day 3

--- day3/day3.rb
+++ day3/day3.rb
@@ -3,26 +3,38 @@ require 'byebug'
 module Day3
   LINE_REGEX = /#(?<id>\d+)\D+(?<left>\d+),(?<top>\d+)\D+(?<w>\d+)x(?<h>\d+)/
 
+  ## Main
   class Day3
-    attr_reader :grid
+    attr_reader :grid, :claims
 
     def initialize
       @input_path = './input.txt'
       @grid = Grid.build_empty_grid(@input_path)
+      @non_overlapping = nil
+      @claims = []
     end
 
     def part_one
+      add_all_claims
+      grid.count_overlaps
+    end
+
+    def part_two
+      add_all_claims
+      claims.find(&:no_overlap?).id
+    end
+
+    def add_all_claims
       File.foreach(@input_path) do |line|
         claim_data = FileUtil.get_claim_data(line)
         claim = Claim.new(claim_data)
-
         claim.add_to_grid(grid)
+        claims << claim
       end
-
-      grid.count_overlaps
     end
   end
 
+  ## Grid
   class Grid
     def self.build_empty_grid(filename)
       max_width, max_height = 0, 0
@@ -36,7 +48,12 @@ module Day3
 
       data_height = max_height + 10 # arbitrary padding
       data_width = max_width + 10 # arbitrary padding
-      data_arr = Array.new(data_height) { Array.new(data_width, 0) }
+
+      # Create an array of arrays, of arrays. The lowest level
+      # array is a bucket to hold coordinates (between 0 - 5 objects)
+      data_arr = Array.new(data_height) do
+        Array.new(data_width) { Array.new }
+      end
 
       self.new(data_arr)
     end
@@ -64,14 +81,22 @@ module Day3
     end
   end
 
+  ## Claim 
   class Claim
-    attr_reader :width, :height, :left, :top
+    attr_reader :width, :height, :left, :top, :id
+    attr_accessor :overlaps
 
     def initialize(claim_data)
       @width = claim_data[:w].to_i
       @height = claim_data[:h].to_i
       @left = claim_data[:left].to_i
       @top = claim_data[:top].to_i
+      @id = claim_data[:id].to_i
+      @overlaps = false
+    end
+
+    def no_overlap?
+      !overlaps
     end
 
     def right_edge
@@ -85,13 +110,34 @@ module Day3
     def add_to_grid(grid)
       (left + 1..right_edge).each do |x_coord|
         (top + 1..bottom_edge).each do |y_coord|
-          pos = [x_coord, y_coord]
-          grid[pos] += 1
+          pos = x_coord, y_coord
+          bucket = grid[pos]
+          bucket << Coordinate.new(x_coord, y_coord, self)
+
+          set_claims_to_overlap!(bucket) if bucket.size > 1
         end
       end
     end
+
+    def set_claims_to_overlap!(bucket)
+      bucket.each(&:set_claim_to_overlap)
+    end
   end
 
+  ## Coord
+  class Coordinate
+    attr_reader :x, :y, :claim
+
+    def initialize(x, y, claim)
+      @x, @y, @claim = x, y, claim
+    end
+
+    def set_claim_to_overlap
+      claim.overlaps = true
+    end
+  end
+
+  ## Util
   class FileUtil
     def self.get_claim_data(claim_text)
       # Example claim_text: 
