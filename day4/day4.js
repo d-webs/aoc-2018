d4494d67db04701b78bc9c9ddfd077a393e3617a
David Webster <david.webster@postmates.com>
2018-12-06 01:45:43 -0800

    Make it more functional

--- day4/day4.js
+++ day4/day4.js
@@ -1,19 +1,54 @@
 const fs = require('fs')
+
+/*
+** Regular Expressions
+*/
 const TIMESTAMP_REGEX = /\[\d+-(?<date>.+)\]/
 const GUARD_REGEX = /\].+#(?<id>\d+).+$/
 
+
 /*
 **
-** Utility functions
+** Functional Programming Util
 **
+*/
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
+const last = obj => obj[obj.length - 1]
+const sortByVal = h => (
+  Object.keys(h).sort((k1, k2) => (
+    h[k1] > h[k2] ? 1 : 
+      h[k1] < h[k2] ? 
+        -1 : 
+        0
+  ))
+)
+
+const buildRange = (
+  start, 
+  end, 
+  arr = []
+) => {
+  return start >= end ? arr : [start, ...buildRange(start + 1, end, arr.slice(1))];
+}
+
+/*
+** File I/O
 */
 const getLines = path => (
   fs.readFileSync(path)
     .toString()
     .split(/\n/)
 )
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
-const last = obj => obj[obj.length - 1]
+
+/*
+** Matching Functions
+*/
+const constructMatcher = regex => line => regex.exec(line) || {}
+const getMatchGroup = property => ({ groups = {} }) => groups[property]
+
+/*
+** Parsing
+*/
 const getMinutes = pipe(
   obj => String.prototype.split.call(obj, ":"),
   last,
@@ -21,60 +56,31 @@ const getMinutes = pipe(
   n => n % 60,
 )
 
-const getHours = pipe(
-  obj => /\s(\d+):/.exec(obj),
-  obj => obj[1],
-  parseInt,
-  n => n / 60,
-  Math.floor
-)
-
-const sortByVal = h => Object.keys(h).sort((k1, k2) => h[k1] > h[k2] ? 1 : (h[k1] < h[k2] ? -1 : 0)) 
-const constructMatcher = regex => line => regex.exec(line) || {}
-const getMatchGroup = property => ({ groups = {} }) => groups[property]
-
 const getGuardMatch = constructMatcher(GUARD_REGEX)
 const getTimeMatch = constructMatcher(TIMESTAMP_REGEX)
 
 const getTime = pipe(getTimeMatch, getMatchGroup('date'))
 const getGuard = pipe(getGuardMatch, getMatchGroup('id'))
 
-const getMinutesArr = (t1, t2) => {
-  let _t1 = t1
-  const allMinutes = []
-  while (_t1 < t2) {
-    let mins = getMinutes(_t1)
-    let hours = getHours(_t1)
-    allMinutes.push(mins)
+/*
+ * Sorting
+ */
+const sortByTime = arr => arr.sort((line1, line2) => {
+  const time1 = getTime(line1)
+  const time2 = getTime(line2)
 
-    mins += 1
-    if (Math.floor(mins / 60)) {
-      hours += 1
-      mins %= 60
-
-      if (hours > 24) console.log('uh oh')
-    }
-
-    _t1 = _t1.replace(/\d+:\d+/, `${hours ? hours : '00'}:${mins}`)
-  }
-
-  return allMinutes
-}
+  if (time1 < time2) return -1
+  if (time1 > time2) return 1
+  return 0
+})
 
 
 /*
-** Part One
+ * 
+ * Main
+ * 
 */
-const partOne = () => {
-  const events = getLines('./input.txt').sort((line1, line2) => {
-    const time1 = getTime(line1)
-    const time2 = getTime(line2)
-
-    if (time1 < time2) return -1
-    if (time1 > time2) return 1
-    return 0
-  })
-
+const partOne = events => {
   let current
   const guards = {}
   for (let i = 0; i < events.length - 1; i++) {
@@ -92,12 +98,12 @@ const partOne = () => {
 
     if (event.includes('asleep')) {
       const guard = guards[current];
-      const start = getTime(event)
-      const end = getTime(events[i + 1])
-      const total = new Date(end) - new Date(start)
+      const start = getMinutes(event)
+      const end = getMinutes(events[i + 1])
+      const total = end - start
       guard.addTime(total)
 
-      const mins = getMinutesArr(event, events[i + 1])
+      const mins = buildRange(start, end)
       for (let i = 0; i < mins.length; i++) {
         const minute = mins[i]
         let minuteCount = guard.minutes[minute]
@@ -135,5 +141,11 @@ class Guard {
   }
 }
 
-partOne()
+console.log(
+  pipe(
+    getLines,
+    sortByTime,
+    partOne
+  )('./input.txt')
+)
 
